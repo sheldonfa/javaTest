a3002fe0d0ad3cec6d31d5262472c14b002f2510
fangxin <727171280@qq.com>
2016-11-22 11:06:06 +0800

    person 上传

---
+++ src/main/java/frame/pojo/Person.java
@@ -0,0 +1,17 @@
+package frame.pojo;
+
+/**
+ * Created by Administrator on 2016/11/21.
+ */
+public class Person {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
