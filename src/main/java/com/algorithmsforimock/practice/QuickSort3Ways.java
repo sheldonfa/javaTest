f397410c4ddf734387d3eb4cf7b64865dfd989e9
fangxin <727171280@qq.com>
2017-03-20 18:41:55 +0800

    三路排序 注释修改

---
+++ src/main/java/com/algorithmsforimock/practice/QuickSort3Ways.java
@@ -0,0 +1,19 @@
+package com.algorithmsforimock.practice;
+
+/**
+ * @author fangxin
+ * @description ${END}
+ * @date 2017/3/20 0020.
+ */
+public class QuickSort3Ways {
+
+    public static void sort(int[] arr){
+        int n = arr.length;
+        quickSort(arr,0,arr.length-1);
+    }
+
+    private static void quickSort(int[] arr, int l, int r) {
+
+    }
+
+}
