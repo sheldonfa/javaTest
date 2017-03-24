8731e3692b6e17333e0cb97812e67dc5a4b104aa
fangxin <727171280@qq.com>
2017-03-24 14:44:55 +0800

    线程异常捕获学习

--- src/main/java/com/javabase/concurrency/c21_3_3/AtomicityTest.java
+++ src/main/java/com/javabase/concurrency/c21_3_3/AtomicityTest.java
@@ -1,10 +1,17 @@
 package com.javabase.concurrency.c21_3_3;//: concurrency/AtomicityTest.java
 import java.util.concurrent.*;
 
+/**
+ * getValue方法中，尽管return i是原子操作，但是缺少同步，可以在i的中间状态被读取
+ */
 public class AtomicityTest implements Runnable {
   private int i = 0;
   public int getValue() { return i; }
-  private synchronized void evenIncrement() { i++; i++; }
+  private synchronized void evenIncrement() {
+    i++;
+    i++;
+    System.out.println("i:"+i);
+ }
   public void run() {
     while(true)
       evenIncrement();
@@ -16,7 +23,7 @@ public class AtomicityTest implements Runnable {
     while(true) {
       int val = at.getValue();
       if(val % 2 != 0) {
-        System.out.println(val);
+        System.out.println("val:"+val);
         System.exit(0);
       }
     }
