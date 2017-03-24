c109928a9c37e677277005534d57b858c1cac0e1
fangxin <727171280@qq.com>
2017-03-24 16:17:30 +0800

    线程异常捕获学习

--- src/main/java/com/javabase/concurrency/c21_3_3/AtomicityTest.java
+++ src/main/java/com/javabase/concurrency/c21_3_3/AtomicityTest.java
@@ -3,6 +3,12 @@ import java.util.concurrent.*;
 
 /**
  * getValue方法中，尽管return i是原子操作，但是缺少同步，可以在i的中间状态被读取
+ *
+ * Brian的同步规则:
+ * 如果你正在写一个变量，他可能接下来将被另一个线程读取，或者正在读取一个上一次已经被另一个线程写过的变量，那么必须使用同步，
+ * 并且，读写线程都必须用相同的监视器锁同步。
+ *
+ * 根据Brian的同步规则，这里的getValue()需要加锁
  */
 public class AtomicityTest implements Runnable {
   private int i = 0;
--- src/main/java/com/javabase/concurrency/c21_3_4/AtomicEvenGenerator.java
+++ src/main/java/com/javabase/concurrency/c21_3_4/AtomicEvenGenerator.java
@@ -1,4 +1,4 @@
-package com.javabase.concurrency;//: concurrency/AtomicEvenGenerator.java
+package com.javabase.concurrency.c21_3_4;//: concurrency/AtomicEvenGenerator.java
 // Atomic classes are occasionally useful in regular code.
 // {RunByHand}
 import com.javabase.concurrency.c21_3_1.EvenChecker;
@@ -7,6 +7,7 @@ import com.javabase.concurrency.c21_3_1.IntGenerator;
 import java.util.concurrent.atomic.*;
 
 public class AtomicEvenGenerator extends IntGenerator {
+  /*特殊情况下才在自己的代码中使用类，通常依赖于锁更安全*/
   private AtomicInteger currentEvenValue =
     new AtomicInteger(0);
   public int next() {
